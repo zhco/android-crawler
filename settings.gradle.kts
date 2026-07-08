ca04b5f57b1279d2adca8656ebac0155db7a9ad0
zhco <luycheung@qq.com>
2026-07-08 18:33:15 +0800

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "通用爬虫"
+include(":app")
