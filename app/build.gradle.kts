f8a354528e534a92a9bca0ac9de17f9862142e01
zhco <luycheung@qq.com>
2026-07-09 11:31:58 +0800

    v2.3: build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "2.2"
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
